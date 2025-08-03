f0066e4cbda610c3df27956fa2148b9a13dc9111
Renato Asterio <renato.asterio@hotmail.com>
2025-08-02 21:13:08 -0300

    finalizando aula 237

--- day_31_flash-card-project-start/main.py
+++ day_31_flash-card-project-start/main.py
@@ -1,8 +1,19 @@
-from tkinter import Canvas, Tk, PhotoImage, Label, LabelFrame, Button
+from tkinter import Canvas, Tk, PhotoImage, Button
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_LANGUAGE = ('Arial', 40, 'italic')
 FONT_TRADUCAO = ('Arial', 60, 'bold')
+
+def generate_word():
+    indice_aleatorio = random.choice(range(len(data_dict)))
+    word_random = data_dict[indice_aleatorio]['French']
+    canvas.itemconfig(traducao_text, text=word_random)
+
+
+data = pd.read_csv('data/french_words.csv')
+data_dict = data.to_dict('records')
 
 #janela
 window = Tk()
@@ -16,19 +27,19 @@ canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 #language
 language_text = canvas.create_text(400,150, text='Franch',font=FONT_LANGUAGE)
 #traducao
-traducao_text = canvas.create_text(400,263, text='Trouve',font=FONT_TRADUCAO)
+traducao_text = canvas.create_text(400,263, text='teste',font=FONT_TRADUCAO)
 canvas.grid(row=0, column=0,columnspan=2)
 
 #botao falhou
 erro = Button()
 img_erro_button = PhotoImage(file="images/wrong.png")
-erro.config(image=img_erro_button)
+erro.config(image=img_erro_button, command=generate_word)
 erro.grid(row=1, column=0)
 
 #botao acertou
 acertou = Button()
 img_acertou_button = PhotoImage(file="images/right.png")
-acertou.config(image=img_acertou_button)
+acertou.config(image=img_acertou_button, command=generate_word)
 acertou.grid(row=1, column=1)
 
 window.mainloop()
