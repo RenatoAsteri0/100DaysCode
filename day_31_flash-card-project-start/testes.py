41d58b78f2bda8bfe1220240c6d520a07f3b0f21
Renato Asterio <renato.asterio@hotmail.com>
2025-08-02 21:13:08 -0300

    finalizando aula 237

--- day_31_flash-card-project-start/testes.py
+++ day_31_flash-card-project-start/testes.py
@@ -1,5 +1,10 @@
+import pandas as pd
 def somar(num,num1):
     return num + num1
 
 somas = (map(somar,(2,3),(1,3)))
 print(list(somas))
+
+data = pd.read_csv('data/french_words.csv')
+data_dict = data.to_dict('records')
+print(data_dict[0::]['French'])
