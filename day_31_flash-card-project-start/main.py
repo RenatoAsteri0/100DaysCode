123b4bf1a359259c62ede77814ec8647a0e62075
RenatoAsteri0 <renato.asterio@hotmail.com>
2025-08-04 22:37:03 -0300

    add learning 241

--- day_31_flash-card-project-start/main.py
+++ day_31_flash-card-project-start/main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import Canvas, Tk, PhotoImage, Button
 import pandas as pd
 import random
@@ -8,21 +9,34 @@ FONT_TRADUCAO = ('Arial', 60, 'bold')
 
 data = pd.read_csv('data/french_words.csv')
 data_dict = data.to_dict('records')
+indice_aleatorio = {}
 
 def generate_word():
+    global indice_aleatorio, ticket_id
+    window.after_cancel(ticket_id)
     indice_aleatorio = random.choice(data_dict)
-    canvas.itemconfig(language_text, text='French')
-    canvas.itemconfig(traducao_text, text=indice_aleatorio['French'])
+    canvas.itemconfig(language_text, text='French', fill='black')
+    canvas.itemconfig(traducao_text, text=indice_aleatorio['French'],fill='black')
+    canvas.itemconfig(background_canvas, image=img_card_front)
+    ticket_id = window.after(3000, func=get_translate)
+
+def get_translate():
+    canvas.itemconfig(language_text,text='English',fill='white')
+    canvas.itemconfig(traducao_text, text=indice_aleatorio['English'], fill='white')
+    canvas.itemconfig(background_canvas, image=img_card_back)
 
 
 #janela
 window = Tk()
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+ticket_id = window.after(3000, func=get_translate)
 
 #canvas front
 canvas = Canvas(height=526, width=800, highlightthickness=0)
 img_card_front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263,image=img_card_front)
+#canvas back
+img_card_back = PhotoImage(file="images/card_back.png")
+background_canvas = canvas.create_image(400, 263,image=img_card_front)
 canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 #language
 language_text = canvas.create_text(400,150, text='',font=FONT_LANGUAGE)
@@ -41,6 +55,5 @@ acertou = Button()
 img_acertou_button = PhotoImage(file="images/right.png")
 acertou.config(image=img_acertou_button, command=generate_word)
 acertou.grid(row=1, column=1)
-
 generate_word()
 window.mainloop()
