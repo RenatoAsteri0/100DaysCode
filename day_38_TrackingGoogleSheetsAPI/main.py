04f25daadf59287f62540c428041e9d355791b6d
Renato Asterio <renato.asterio@hotmail.com>
2025-09-15 22:18:18 -0300

    add

--- day_38_TrackingGoogleSheetsAPI/main.py
+++ day_38_TrackingGoogleSheetsAPI/main.py
@@ -1,14 +1,19 @@
 import requests
+import datetime
+import os
+from dotenv import find_dotenv, load_dotenv
 
 MEU_PESO = '77'
 MINHA_ALTURA = '168'
 IDADE = '23'
+doentv_path = find_dotenv()
+load_dotenv(doentv_path)
 
 nutrition_headers = {
-    'x-app-id': '83055098',
-    'x-app-key': 'd0e238bb164069a3cf5aea95f40d4b0e'
+    'x-app-id': os.getenv('API_ID'),
+    'x-app-key': os.getenv('API_KEY')
 }
-exercice = input('digite quais exercicios voce fez: ')
+exercice = 'i ran for 10 miles'
 nutrition_params = {
     'query': exercice,
     'weight_kg': MEU_PESO,
@@ -17,5 +22,26 @@ nutrition_params = {
 }
 nutrition_url = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
-response = requests.post(url=nutrition_url, json=nutrition_params, headers=nutrition_headers)
-print(response.json())
+nutrition_response = requests.post(url=nutrition_url, json=nutrition_params, headers=nutrition_headers)
+result = nutrition_response.json()
+
+hoje = datetime.datetime.now()
+time = hoje.time().strftime('%H:%M:%S')
+data = hoje.date().strftime('%Y-%m-%d')
+googleshets_url = 'https://api.sheety.co/998db4b1cd9676aab0abe91eb37faacb/meusExercicios/workouts'
+
+
+barear_auth = {
+    "Authorization": 'Bearer renatotoken'
+}
+for exercices in result['exercises']:
+    googleshets_colums = {
+        'workout': {
+            'date': data,
+            'time': time,
+            'exercice': exercices['name'],
+            'duration': exercices['duration_min'],
+            'calories': exercices['nf_calories']
+        }
+    }
+    exercice_response = requests.post(url=googleshets_url, json=googleshets_colums, headers=barear_auth)
