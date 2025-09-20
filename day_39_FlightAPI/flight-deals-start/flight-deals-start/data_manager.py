ebbde3f7cf36ce7782290a3412c3481b45365d9e
RenatoAsteri0 <renato.asterio@hotmail.com>
2025-09-19 23:23:50 -0300

    Adicionando todos os meus aprendizados com o curso

--- day_39_FlightAPI/flight-deals-start/flight-deals-start/data_manager.py
+++ day_39_FlightAPI/flight-deals-start/flight-deals-start/data_manager.py
@@ -1,7 +1,28 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SHEETY_PRICES_ENDPOINT = 'https://api.sheety.co/998db4b1cd9676aab0abe91eb37faacb/flightDeals/prices'
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
-import requests
-headers = {'Authorization': 'Bearer APIKeySheetFlight'}
-response = requests.get('https://api.sheety.co/998db4b1cd9676aab0abe91eb37faacb/flightDeals/prices', headers=headers)
-print(response.json())
+    def __init__(self):
+        self.headers = {'Authorization': os.getenv('APIBearer_Sheety')}
+        self.destination_data = {}
+
+    def get_sheed_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=self.headers)
+        data = response.json()
+        self.destination_data = data['prices']
+        return self.destination_data
+
+    def put_sheet_data(self):
+        for city in self.destination_data:
+            new_data = {
+                'price': {
+                    'iataCode': city['iataCode']
+                }
+            }
+            response = requests.put(f"{SHEETY_PRICES_ENDPOINT}/{city['id']}", headers=self.headers, json=new_data)
+            print(response.text)
--- day_39_FlightAPI/flight-deals-start/flight-deals-start/main.py
+++ day_39_FlightAPI/flight-deals-start/flight-deals-start/main.py
@@ -1,3 +1,17 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
+import pprint
 
-import notification_manager, data_manager, flight_data, flight_search
+data_manager = DataManager()
+sheet_data = data_manager.get_sheed_data()
+print(sheet_data)
+
+if sheet_data[0]['iataCode'] == '':
+    flighsearch = FlightSearch()
+    for row in sheet_data:
+        row['iataCode'] = flighsearch.get_destination_code(row['city'])
+    print(f"sheet_data:\n {sheet_data}")
+
+    data_manager.destination_data = sheet_data
+    data_manager.put_sheet_data()
