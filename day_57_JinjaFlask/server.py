4d8d494d0d110262dd0e6b6a66b288e4bd165674
renato <renato.asterio@hotmail.com>
2026-01-31 17:09:38 -0300

    add

--- day_57_JinjaFlask/server.py
+++ day_57_JinjaFlask/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -9,8 +10,20 @@ def home():
 
 @app.route('/guess/<name>')
 def guess(name):
-    name = 'renato'
-    return render_template(template_name_or_list='guess.html'), name
+    get_gender = requests.get(url=f"https://api.genderize.io?name={name}")
+    gender = get_gender.json()['gender']
+
+    get_age = requests.get(url=f"https://api.agify.io?name={name}")
+    age = get_age.json()['age']
+
+    return render_template(template_name_or_list='guess.html', name=name, age=age, gender=gender)
+
+@app.route('/blog')
+def blog():
+    response = requests.get(url='https://api.npoint.io/c790b4d5cab58020d391')
+    all_blogs = response.json()
+    return render_template(template_name_or_list='blog.html',blogs=all_blogs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- day_57_JinjaFlask/test.py
+++ day_57_JinjaFlask/test.py
@@ -3,14 +3,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-APIKEY = os.getenv('AGIFY_GENDERIZE')
 end_point = 'https://api.genderize.io'
 parameters = {
     'name': 'renato',
-    'country_id': 'US',
-    'apikey': APIKEY
+    'country_id': 'US'
 }
 
 responde = requests.get(url=end_point, params=parameters)
-print(responde)
+print(responde.json()['gender'])
