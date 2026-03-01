e5193735424f87e27fb66e470dc8c6588d014428
RenatoAsteri0 <renato.asterio@hotmail.com>
2026-03-01 11:52:13 -0300

    add

--- day_57_JinjaFlask/day-57-starting-files-blog-templating/main.py
+++ day_57_JinjaFlask/day-57-starting-files-blog-templating/main.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
+posts = requests.get(url='https://api.npoint.io/c790b4d5cab58020d391').json()
+post_object = []
+
+for post in posts:
+    post_obj = Post(id=post['id'], body=post['body'], title=post['title'], subtitle=post['subtitle'])
+    post_object.append(post_obj)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    response = requests.get(url='https://api.npoint.io/c790b4d5cab58020d391')
-    blogs = response.json()
-    return render_template("index.html", blogs=blogs)
+    return render_template("index.html", blogs=post_object)
 
-@app.route('/posts/<idnum>')
+@app.route('/posts/<int:idnum>')
 def get_post(idnum):
-    return render_template('posts.html')
+    requested = None
+    for blog_post in post_object:
+        if blog_post.id == idnum:
+            requested = blog_post
+    return render_template('post.html', post=requested)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- day_57_JinjaFlask/day-57-starting-files-blog-templating/post.py
+++ day_57_JinjaFlask/day-57-starting-files-blog-templating/post.py
@@ -1,2 +1,6 @@
 class Post:
-    pass
+    def __init__(self, id, body, title, subtitle):
+        self.id = id
+        self.body = body
+        self.title = title
+        self.subtitle = subtitle
