091623cb1a0014ca41d6ecb5d799489d6481eec2
renato <renato.asterio@hotmail.com>
2026-03-01 14:01:18 -0300

    add

--- day_60_CreatinAForm/html_forms/main.py
+++ day_60_CreatinAForm/html_forms/main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def receive_data():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        return render_template("login.html", username=username, password=password)
 
 if __name__ == "__main__":
     app.run(debug=True)
